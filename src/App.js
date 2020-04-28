1a6728d71b655753d82a4d9304c5e0942efa82ba
饶家俊 <raojiajun@bimwinner.com>
2020-04-28 22:32:32 +0800

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2020年04月28日22:32:26
         </a>
       </header>
     </div>
